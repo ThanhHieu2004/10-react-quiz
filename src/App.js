12f8e2764e49082f10f16076931b1674f0ae7884
Thanh Hieu <thanhhieu8304@gmail.com>
2025-09-15 08:51:51 +0700

    Add JSON data for quiz questions and implement JSON server
    - Created a new JSON file `questions.json` containing a list of quiz questions, options, correct answers, and points.
    - Updated `package.json` to include `json-server` for serving the questions data.
    - Added a new script to start the JSON server on port 8000.
    - Modified `App.js` to fetch questions from the JSON server using `useReducer` and `useEffect` for state management and data fetching.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'activate', 'finish'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived": {
+      console.log(action.payload);
+      return { ...state, questions: action.payload, status: "ready" };
+    }
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Main>
-        <p>1/15</p>
+        <p></p>
         <p>Question</p>
       </Main>
     </div>
